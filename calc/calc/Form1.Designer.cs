bb75d994654fff03919812f148f4550322469995
PRIANTE SEBASTIANO <12999516@itisrossi.edu.it>
2025-02-22 09:46:02 +0100

    a

---
+++ calc/calc/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_storico = new System.Windows.Forms.Label();
+            this.lbl_calcoli = new System.Windows.Forms.Label();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_vuoto = new System.Windows.Forms.Button();
+            this.btn_uguale = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_less = new System.Windows.Forms.Button();
+            this.btn_divide = new System.Windows.Forms.Button();
+            this.btn_times = new System.Windows.Forms.Button();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_storico
+            // 
+            this.lbl_storico.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_storico.Location = new System.Drawing.Point(30, 9);
+            this.lbl_storico.Name = "lbl_storico";
+            this.lbl_storico.Size = new System.Drawing.Size(400, 60);
+            this.lbl_storico.TabIndex = 0;
+            this.lbl_storico.Text = "labelò";
+            this.lbl_storico.Click += new System.EventHandler(this.lbl_storico_Click);
+            // 
+            // lbl_calcoli
+            // 
+            this.lbl_calcoli.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_calcoli.Location = new System.Drawing.Point(30, 106);
+            this.lbl_calcoli.Name = "lbl_calcoli";
+            this.lbl_calcoli.Size = new System.Drawing.Size(400, 60);
+            this.lbl_calcoli.TabIndex = 1;
+            this.lbl_calcoli.Text = "labelù";
+            this.lbl_calcoli.Click += new System.EventHandler(this.lbl_calcoli_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_7.Location = new System.Drawing.Point(34, 210);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(75, 75);
+            this.btn_7.TabIndex = 2;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_9.Location = new System.Drawing.Point(233, 210);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(75, 75);
+            this.btn_9.TabIndex = 3;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4.Location = new System.Drawing.Point(34, 308);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 75);
+            this.btn_4.TabIndex = 4;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_8.Location = new System.Drawing.Point(131, 210);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(75, 75);
+            this.btn_8.TabIndex = 5;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_5.Location = new System.Drawing.Point(131, 308);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(75, 75);
+            this.btn_5.TabIndex = 6;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(34, 399);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 75);
+            this.btn_1.TabIndex = 7;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2.Location = new System.Drawing.Point(131, 399);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 75);
+            this.btn_2.TabIndex = 8;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3.Location = new System.Drawing.Point(233, 399);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 75);
+            this.btn_3.TabIndex = 9;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_6.Location = new System.Drawing.Point(233, 308);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(75, 75);
+            this.btn_6.TabIndex = 10;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_vuoto
+            // 
+            this.btn_vuoto.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_vuoto.Location = new System.Drawing.Point(34, 489);
+            this.btn_vuoto.Name = "btn_vuoto";
+            this.btn_vuoto.Size = new System.Drawing.Size(75, 75);
+            this.btn_vuoto.TabIndex = 11;
+            this.btn_vuoto.UseVisualStyleBackColor = true;
+            // 
+            // btn_uguale
+            // 
+            this.btn_uguale.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_uguale.Location = new System.Drawing.Point(233, 489);
+            this.btn_uguale.Name = "btn_uguale";
+            this.btn_uguale.Size = new System.Drawing.Size(75, 75);
+            this.btn_uguale.TabIndex = 12;
+            this.btn_uguale.Text = "=";
+            this.btn_uguale.UseVisualStyleBackColor = true;
+            this.btn_uguale.Click += new System.EventHandler(this.btn_uguale_Click);
+            // 
+            // btn_0
+            // 
+            this.btn_0.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0.Location = new System.Drawing.Point(131, 489);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(75, 75);
+            this.btn_0.TabIndex = 13;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_less
+            // 
+            this.btn_less.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_less.Location = new System.Drawing.Point(355, 308);
+            this.btn_less.Name = "btn_less";
+            this.btn_less.Size = new System.Drawing.Size(75, 75);
+            this.btn_less.TabIndex = 14;
+            this.btn_less.Text = "-";
+            this.btn_less.UseVisualStyleBackColor = true;
+            this.btn_less.Click += new System.EventHandler(this.btn_less_Click);
+            // 
+            // btn_divide
+            // 
+            this.btn_divide.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_divide.Location = new System.Drawing.Point(355, 489);
+            this.btn_divide.Name = "btn_divide";
+            this.btn_divide.Size = new System.Drawing.Size(75, 75);
+            this.btn_divide.TabIndex = 15;
+            this.btn_divide.Text = "÷";
+            this.btn_divide.UseVisualStyleBackColor = true;
+            this.btn_divide.Click += new System.EventHandler(this.btn_divide_Click);
+            // 
+            // btn_times
+            // 
+            this.btn_times.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_times.Location = new System.Drawing.Point(355, 399);
+            this.btn_times.Name = "btn_times";
+            this.btn_times.Size = new System.Drawing.Size(75, 75);
+            this.btn_times.TabIndex = 16;
+            this.btn_times.Text = "X";
+            this.btn_times.UseVisualStyleBackColor = true;
+            this.btn_times.Click += new System.EventHandler(this.btn_times_Click);
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_plus.Location = new System.Drawing.Point(355, 210);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(75, 75);
+            this.btn_plus.TabIndex = 17;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.Location = new System.Drawing.Point(355, 579);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 75);
+            this.btn_delete.TabIndex = 18;
+            this.btn_delete.Text = "⌫";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 721);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.btn_times);
+            this.Controls.Add(this.btn_divide);
+            this.Controls.Add(this.btn_less);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_uguale);
+            this.Controls.Add(this.btn_vuoto);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.lbl_calcoli);
+            this.Controls.Add(this.lbl_storico);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_storico;
+        private System.Windows.Forms.Label lbl_calcoli;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_vuoto;
+        private System.Windows.Forms.Button btn_uguale;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_less;
+        private System.Windows.Forms.Button btn_divide;
+        private System.Windows.Forms.Button btn_times;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Button btn_delete;
+    }
+}
+
---
+++ calc/calc/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public partial class Form1 : Form
+    {
+        int somm = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            agg_UI("0");
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            agg_UI("1");
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "2";
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "3";
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "4";
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "5";
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "6";
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "7";
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "8";
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            lbl_calcoli.Text = "9";
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_less_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_times_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_divide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_uguale_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_calcoli_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_storico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void agg_UI(string num)
+        {
+            string num1 = "";
+
+            lbl_calcoli.Text = $"{num1}{num}";
+
+            num1 = num;
+        }
+    }
+}
